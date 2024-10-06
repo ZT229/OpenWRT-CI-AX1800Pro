99da50696eca1e3af246e361ec310c2e576ccf94
ZT229 <2432702069@qq.com>
2024-10-06 17:23:40 +0800

    修改

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -2,4 +2,4 @@
 
 # 替换成最新版的golang
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
